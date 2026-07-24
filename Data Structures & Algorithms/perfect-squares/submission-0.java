7da524a2a7d90e0b68e80115e7f7af7dbb1fa9a9
Shiven Goomer <shivengoomer@gmail.com>
2026-07-24 16:56:04 +0530

    Add: perfect-squares - submission-0

---
+++ Data Structures & Algorithms/perfect-squares/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,n);
+        dp[0]=0;
+        for(int i=0;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                dp[i]=Math.min(dp[i],1+dp[i-j*j]);
+            }
+        }
+        return dp[n];
+    }
+}
